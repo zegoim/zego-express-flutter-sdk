e590af2db4d3306ae4a27b2cb669e899d73648b2
Patrick Fu <patrickfu@zego.im>
2020-03-25 18:25:30 +0800

    feat: Added log method for iOS

---
+++ ios/Classes/ZegoLog.h
@@ -0,0 +1,27 @@
+//
+//  ZegoLog.h
+//  Pods-Runner
+//
+//  Created by Patrick Fu on 2020/3/25.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZegoLog : NSObject
+
+#define ZGLog(frmt, ...) \
+[ZegoLog logWithFile:__FILE__ \
+function:__PRETTY_FUNCTION__ \
+line:__LINE__ \
+format:(frmt), ## __VA_ARGS__]
+
++ (void)logWithFile:(const char *)file
+           function:(const char *)function
+               line:(NSUInteger)line
+             format:(NSString *)format, ... NS_FORMAT_FUNCTION(4,5);
+
+@end
+
+NS_ASSUME_NONNULL_END
