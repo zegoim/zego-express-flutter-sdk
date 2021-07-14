ae25704b1ee559d7e1731c5d5f23e6dc89cb4def
Patrick Fu <patrickfu@zego.im>
2021-07-15 00:12:04 +0800

    🔥 Remove test code in the example project

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -11,17 +11,5 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:zego_express_engine_example/main.dart';
 
 void main() {
-  testWidgets('Verify Platform version', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
 
-    // Verify that platform version is retrieved.
-    expect(
-      find.byWidgetPredicate(
-        (Widget widget) => widget is Text &&
-                           widget.data.startsWith('Running on:'),
-      ),
-      findsOneWidget,
-    );
-  });
 }
