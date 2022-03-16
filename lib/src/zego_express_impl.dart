886008e72d84b8885a6e84eab0d6e7cb33d09552
erchizeng <erchizeng@zego.im>
2022-03-16 09:44:19 +0800

    ⬆️ Upgrade version 2.17.1

--- lib/src/zego_express_impl.dart
+++ lib/src/zego_express_impl.dart
@@ -7,7 +7,7 @@ import 'zego_express_api.dart';
 import 'zego_express_defines.dart';
 
 class Global {
-  static String pluginVersion = "2.17.0";
+  static String pluginVersion = "2.17.1";
 }
 
 class ZegoExpressImpl {
