4889fb77ad6b58725ad10d2369960ac7283d771d
erchizeng <erchizeng@zego.im>
2022-04-14 20:03:27 +0800

    📝 Update Global pluginVersion to 2.17.2

--- lib/src/zego_express_impl.dart
+++ lib/src/zego_express_impl.dart
@@ -7,7 +7,7 @@ import 'zego_express_api.dart';
 import 'zego_express_defines.dart';
 
 class Global {
-  static String pluginVersion = "2.17.1";
+  static String pluginVersion = "2.17.2";
 }
 
 class ZegoExpressImpl {
