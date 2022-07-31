59f03f3aa7f52c44a9ba74a884c2a9836bb67439
Patrick Fu <paaatrickfu@gmail.com>
2022-07-31 23:45:52 +0800

    🚩 Add custom video capture manager for macOS

---
+++ macos/Classes/ZegoCustomVideoCaptureManager.h
@@ -0,0 +1,74 @@
+//
+//  ZegoCustomVideoCaptureManager.h
+//  zego_express_engine
+//
+//  Created by Patrick Fu on 2022/7/18.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreVideo/CoreVideo.h>
+#import <CoreMedia/CoreMedia.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol ZegoFlutterCustomVideoCaptureHandler <NSObject>
+
+@required
+
+/// The callback triggered when the SDK is ready to receive captured video data. Only those video data that are sent to the SDK after this callback is received are valid.
+///
+/// @param channel publish channel, It is consistent with Dart API
+- (void)onStart:(int)channel;
+
+/// The callback triggered when SDK stops receiving captured video data.
+///
+/// After receiving this callback, the developer is advised to set the saved client object to nil, otherwise the memory occupied by the client object will always exist
+/// @param channel publish channel, It is consistent with Dart API
+- (void)onStop:(int)channel;
+
+@end
+
+@interface ZegoCustomVideoCaptureManager : NSObject
+
+/// Get the custom video capture manager instance
++ (instancetype)sharedInstance;
+
+/// Sets the event callback handler for custom video capture.
+///
+/// Developers need to pass in the callback object that implements [ZegoFlutterCustomVideoCaptureHandler] and open [enableCustomVideoCapture] in the Dart API to make the custom capture module take effect.
+/// When the developer calls [startPreview]/[stopPreview] or [startPublishingStream]/[stopPublishingStream] in the Dart API, the SDK will notify the developer  the start/stop of the custom video capture, and the developer can receive [onStart] and [onStop] Start and stop the external input source after the notification.
+/// This API call is set at any time before [enableCustomVideoCapture] is enabled in Dart
+/// @param handler the callback object that implements the [ZegoFlutterCustomVideoCaptureHandler]
+- (void)setCustomVideoCaptureHandler:(id<ZegoFlutterCustomVideoCaptureHandler>)handler;
+
+
+/// Sets the video mirroring mode.
+///
+/// This function can be called to set whether the local preview video and the published video have mirror mode enabled.
+///
+/// @param mode Mirror mode for previewing or publishing the stream. 0: Only preview mirror. 1: Both preview and publish mirror. 2: No Mirror. 3. Only publish mirror. No mirror by default.
+/// @param channel publish channel, It is consistent with Dart API
+- (void)setVideoMirrorMode:(int)mode channel:(int)channel;
+
+/// Sends the video frames (CVPixelBuffer) produced by custom video capture to the SDK.
+///
+/// This api need to be start called after the [onStart] callback notification and to be stop called call after the [onStop] callback notification.
+///
+/// @param buffer Video frame data to send to the SDK
+/// @param timestamp Timestamp of this video frame
+/// @param channel publish channel, It is consistent with Dart API
+- (void)sendCVPixelBuffer:(CVPixelBufferRef)buffer timestamp:(CMTime)timestamp channel:(int)channel;
+
+/// Sends the video frames (Texture Data) produced by custom video capture to the SDK.
+///
+/// This api need to be start called after the [onStart] callback notification and to be stop called call after the [onStop] callback notification.
+///
+/// @param textureID texture ID
+/// @param size Video frame width and height
+/// @param timestamp Timestamp of this video frame
+/// @param channel publish channel, It is consistent with Dart API
+- (void)sendGLTextureData:(GLuint)textureID size:(CGSize)size timestamp:(CMTime)timestamp channel:(int)channel;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ macos/Classes/ZegoCustomVideoCaptureManager.m
@@ -0,0 +1,78 @@
+//
+//  ZegoCustomVideoCaptureManager.m
+//  zego_express_engine
+//
+//  Created by Patrick Fu on 2022/7/18.
+//
+
+#import "ZegoCustomVideoCaptureManager.h"
+#import "ZegoTextureRendererController.h"
+#import "ZegoExpressEngineMethodHandler.h"
+#import "ZegoLog.h"
+#import <ZegoExpressEngine/ZegoExpressEngine.h>
+
+@interface ZegoCustomVideoCaptureManager()<ZegoCustomVideoCaptureHandler>
+
+@property (nonatomic, weak) id<ZegoFlutterCustomVideoCaptureHandler> handler;
+
+@property (nonatomic, assign)ZegoVideoMirrorMode mirrorMode;
+@property (nonatomic, strong)ZegoVideoFrameParam *videoParam;
+@end
+
+@implementation ZegoCustomVideoCaptureManager
+
++ (instancetype)sharedInstance {
+    static ZegoCustomVideoCaptureManager *instance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        instance = [[ZegoCustomVideoCaptureManager alloc] init];
+    });
+    return instance;
+}
+
+- (void)setCustomVideoCaptureHandler:(id<ZegoFlutterCustomVideoCaptureHandler>)handler {
+    
+    self.handler = handler;
+}
+
+- (void)setVideoMirrorMode:(int)mode channel:(int)channel{
+    _mirrorMode = mode;
+    if([ZegoExpressEngineMethodHandler sharedInstance].enablePlatformView) {
+        [[ZegoExpressEngine sharedEngine] setVideoMirrorMode:_mirrorMode channel:channel];
+    }
+}
+
+- (void)sendCVPixelBuffer:(CVPixelBufferRef)buffer timestamp:(CMTime)timestamp channel:(int)channel{
+    [[ZegoExpressEngine sharedEngine] sendCustomVideoCapturePixelBuffer:buffer timestamp:timestamp];
+    // 使用 Texture 方式渲染时，还需要将数据传给 TextureRednerer
+    if(![ZegoExpressEngineMethodHandler sharedInstance].enablePlatformView) {
+        [[ZegoTextureRendererController sharedInstance] onCapturedVideoFrameCVPixelBuffer:buffer param:self.videoParam flipMode:(_mirrorMode == ZegoVideoMirrorModeOnlyPreviewMirror || _mirrorMode == ZegoVideoMirrorModeBothMirror) channel:channel];
+    }
+}
+
+- (void)sendGLTextureData:(GLuint)textureID size:(CGSize)size timestamp:(CMTime)timestamp channel:(int)channel {
+    [[ZegoExpressEngine sharedEngine] sendCustomVideoCaptureTextureData:textureID size:size timestamp:timestamp channel:(ZegoPublishChannel)channel];
+    // 使用 Texture 方式渲染时，此方法无法直接渲染
+    // TODO: 考虑使用 gltexture -> cvpixelbuffer 的转换
+}
+
+# pragma mark ZegoCustomVideoCaptureHandler
+- (void)onStart:(ZegoPublishChannel)channel {
+    ZGLog(@"[CustomVideoCapture] onStart");
+
+    if([self.handler respondsToSelector:@selector(onStart:)]) {
+        [self.handler onStart:(int)channel];
+    }
+}
+
+
+- (void)onStop:(ZegoPublishChannel)channel {
+    ZGLog(@"[CustomVideoCapture] onStop");
+    
+    if([self.handler respondsToSelector:@selector(onStop:)]) {
+        [self.handler onStop:(int)channel];
+    }
+}
+
+@end
+
