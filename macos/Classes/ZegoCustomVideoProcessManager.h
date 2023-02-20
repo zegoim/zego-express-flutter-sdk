ac022bae71c10f6a0b3d9eec7e7dc6722db90890
erchizeng <erchizeng@zego.im>
2023-02-20 14:12:23 +0800

    ✨ Add mac CustomVideoProcess CustomVideoRender and MediaPlayer setVideoHandle

---
+++ macos/Classes/ZegoCustomVideoProcessManager.h
@@ -0,0 +1,97 @@
+//
+//  ZegoCustomVideoProcessManager.h
+//  Pods
+//
+//  Created by 27 on 2023/2/2.
+//
+
+#ifndef ZegoCustomVideoProcessManager_h
+#define ZegoCustomVideoProcessManager_h
+
+#import <Foundation/Foundation.h>
+#import <CoreVideo/CoreVideo.h>
+#import <CoreMedia/CoreMedia.h>
+#import "ZegoCustomVideoDefine.h"
+
+@protocol ZegoFlutterCustomVideoProcessHandler <NSObject>
+
+@optional
+
+/// The SDK informs the developer that it is about to start custom video processing.
+///
+/// Available since: 2.2.0
+/// Description: When the custom video pre-processing is turned on, the SDK informs the developer that the video pre-processing is about to start, and it is recommended to initialize other resources(E.g. Beauty SDK) in this callback.
+/// Use cases: After the developer collects the video data by himself or obtains the video data collected by the SDK, if the basic beauty and watermark functions of the SDK cannot meet the needs of the developer (for example, the beauty effect cannot meet the expectations), the ZegoEffects SDK can be used to perform the video Some special processing, such as beautifying, adding pendants, etc., this process is the pre-processing of custom video.
+/// When to Trigger: Open custom video pre-processing, after calling [startPreview] or [startPublishingStream] successfully.
+/// Related callbacks: Custom video pre-processing end notification [onStop].
+/// Related APIs: Call [setCustomVideoProcessHandler] function to set custom video pre-processing callback.
+///
+/// @param channel Publishing stream channel.
+- (void)onStart:(ZGFlutterPublishChannel)channel;
+
+/// The SDK informs the developer to stop custom video processing.
+///
+/// Available since: 2.2.0
+/// Description: When the custom video pre-processing is turned on, the SDK informs the developer that the video pre-processing is about to end, and it is recommended to destroy other resources(E.g. Beauty SDK) in this callback.
+/// Use cases: After the developer collects the video data by himself or obtains the video data collected by the SDK, if the basic beauty and watermark functions of the SDK cannot meet the needs of the developer (for example, the beauty effect cannot meet the expectations), the ZegoEffects SDK can be used to perform the video Some special processing, such as beautifying, adding pendants, etc., this process is the pre-processing of custom video.
+/// When to Trigger: If you call [startPreview] to start the preview and [startPublishingStream] to start the push stream at the same time after you start the custom capture, you should call [stopPreview] to stop the preview and [stopPublishingStream] to stop the push stream before the callback will be triggered.
+/// Related callbacks: Custom video pre-processing start notification [onStart].
+/// Related APIs: Call [setCustomVideoProcessHandler] function to set custom video pre-processing callback.
+///
+/// @param channel Publishing stream channel.
+- (void)onStop:(ZGFlutterPublishChannel)channel;
+
+/// Call back when the original video data of type [CVPixelBuffer] is obtained.
+///
+/// Available since: 2.2.0
+/// Description: When the custom video pre-processing is turned on, after calling [setCustomVideoProcessHandler] to set the callback, the SDK receives the original video data and calls back to the developer. After the developer has processed the original image, he must call [sendCustomVideoProcessedCVPixelbuffer] to send the processed data back to the SDK, otherwise it will cause frame loss.
+/// Use cases: After the developer collects the video data by himself or obtains the video data collected by the SDK, if the basic beauty and watermark functions of the SDK cannot meet the needs of the developer (for example, the beauty effect cannot meet the expectations), the ZegoEffects SDK can be used to perform the video Some special processing, such as beautifying, adding pendants, etc., this process is the pre-processing of custom video.
+/// When to Trigger: When the custom video pre-processing is enabled, the SDK collects the original video data.
+/// Restrictions: This interface takes effect when [enableCustomVideoProcessing] is called to enable custom video pre-processing and the bufferType of config is passed in [ZegoVideoBufferTypeCVPixelBuffer].
+/// Platform differences: It only takes effect on the iOS/macOS platform.
+///
+/// @param buffer CVPixelBuffer type video frame data to be sent to the SDK.
+/// @param timestamp Timestamp of this video frame.
+/// @param channel Publishing stream channel.
+- (void)onCapturedUnprocessedCVPixelBuffer:(CVPixelBufferRef)buffer
+                                 timestamp:(CMTime)timestamp
+                                   channel:(ZGFlutterPublishChannel)channel;
+
+@end
+
+@interface ZegoCustomVideoProcessManager : NSObject
+
+/// Get the custom video capture manager instance
++ (instancetype)sharedInstance;
+
+/// Sets up the event callback handler for custom video processing.
+///
+/// Available since: 2.2.0 (Android/iOS/macOS native), 2.4.0 (Windows/macOS C++).
+/// Description: When the developer opens the custom pre-processing, by calling [setCustomVideoProcessHandler], you can set the custom video pre-processing callback to obtain the original video data.
+/// Use cases: After the developer collects the video data by himself or obtains the video data collected by the SDK, if the basic beauty and watermark functions of the SDK cannot meet the needs of the developer (for example, the beauty effect cannot meet the expectations), the ZegoEffects SDK can be used to perform the video Some special processing, such as beautifying, adding pendants, etc., this process is the pre-processing of custom video.
+/// When to call: Called before [startPreview], [startPublishingStream] and [createRealTimeSequentialDataManager], otherwise it may cause the timing of obtaining video data to be too slow.
+/// Restrictions: None.
+/// Related APIs: Call [enableCustomVideoProcessing] function to enable custom video pre-processing callback.
+/// Note: This function is only available in ZegoExpressVideo SDK!
+///
+/// @param handler Custom video process handler.Required: Yes.
+- (void)setCustomVideoProcessHandler:(id<ZegoFlutterCustomVideoProcessHandler>)handler;
+
+
+/// Send the [CVPixelBuffer] type video data after the custom video processing to the SDK (for the specified channel).
+///
+/// Available since: 2.2.0 (iOS native), 2.4.0 (macOS C++).
+/// Description: When the custom video pre-processing is turned on, the [CVPixelBuffer] format video data after the custom video pre-processing is sent to the SDK, and other channels are supported.
+/// Use cases: After the developer collects the video data by himself or obtains the video data collected by the SDK, if the basic beauty and watermark functions of the SDK cannot meet the needs of the developer (for example, the beauty effect cannot meet the expectations), the ZegoEffects SDK can be used to perform the video Some special processing, such as beautifying, adding pendants, etc., this process is the pre-processing of custom video.
+/// When to call: Must be called in the [onCapturedUnprocessedCVPixelbuffer] callback.
+/// Restrictions: This interface takes effect when [enableCustomVideoProcessing] is called to enable custom video pre-processing and the bufferType of config is passed in [ZegoVideoBufferTypeCVPixelBuffer].
+/// Platform differences: Only valid on Windows platform.
+/// Note: This function is only available in ZegoExpressVideo SDK!
+///
+/// @param buffer CVPixelBuffer type video frame data to be sent to the SDK.Required: Yes.
+/// @param timestamp Timestamp of this video frame.Required: Yes.
+- (void)sendProcessedCVPixelBuffer:(CVPixelBufferRef)buffer timestamp:(CMTime)timestamp channel:(ZGFlutterPublishChannel)channel;
+
+@end
+
+#endif /* ZegoCustomVideoProcessManager_h */
---
+++ macos/Classes/ZegoCustomVideoRenderManager.h
@@ -0,0 +1,131 @@
+//
+//  ZegoCustomVideoRenderManager.h
+//  Pods
+//
+//  Created by 27 on 2023/2/2.
+//
+
+#ifndef ZegoCustomVideoRenderManager_h
+#define ZegoCustomVideoRenderManager_h
+
+#import <Foundation/Foundation.h>
+#import <CoreVideo/CoreVideo.h>
+#import <CoreMedia/CoreMedia.h>
+#import "ZegoCustomVideoDefine.h"
+
+@protocol ZegoFlutterCustomVideoRenderHandler <NSObject>
+
+@optional
+
+/// When custom video rendering is enabled, the original video frame data collected by the local preview is called back.
+///
+/// Available since: 1.1.0
+/// Description: When using custom video rendering, the SDK callbacks the original video frame data collected by the local preview, which is rendered by the developer.
+/// Use cases: Use a cross-platform interface framework or game engine; need to obtain the video frame data collected or streamed by the SDK for special processing.
+/// When to Trigger: When the local preview is turned on, when the SDK collects the local preview video frame data.
+/// Related APIs: Call [setCustomVideoRenderHandler] to set custom video rendering callback.
+///
+/// @param data Raw video frame data (eg: RGBA only needs to consider data[0], I420 needs to consider data[0,1,2]).
+/// @param dataLength Data length (eg: RGBA only needs to consider dataLength[0], I420 needs to consider dataLength[0,1,2]).
+/// @param param Video frame parameters.
+/// @param flipMode video flip mode.
+/// @param channel Publishing stream channel.
+- (void)onCapturedVideoFrameRawData:(unsigned char *_Nonnull *_Nonnull)data
+                         dataLength:(unsigned int *)dataLength
+                              param:(ZGFlutterVideoFrameParam *)param
+                           flipMode:(ZGFlutterVideoFlipMode)flipMode
+                            channel:(ZGFlutterPublishChannel)channel;
+
+/// When custom video rendering is enabled, the remote end pulls the original video frame data to call back, and distinguishes different streams by streamID.
+///
+/// Available since: 1.1.0
+/// Description: When custom video rendering is enabled, the SDK calls back the remote end to pull the original video frame data, distinguishes different streams by streamID, and renders them by the developer.
+/// Use cases: Use a cross-platform interface framework or game engine; need to obtain the video frame data collected or streamed by the SDK for special processing.
+/// When to Trigger: After starting to stream, when the SDK receives the video frame data of the remote stream.
+/// Related APIs: Call [setCustomVideoRenderHandler] to set custom video rendering callback.
+///
+/// @param data Raw video frame data (eg: RGBA only needs to consider data[0], I420 needs to consider data[0,1,2]).
+/// @param dataLength Data length (eg: RGBA only needs to consider dataLength[0], I420 needs to consider dataLength[0,1,2]).
+/// @param param Video frame parameters.
+/// @param streamID Stream ID.
+- (void)onRemoteVideoFrameRawData:(unsigned char *_Nonnull *_Nonnull)data
+                       dataLength:(unsigned int *)dataLength
+                            param:(ZGFlutterVideoFrameParam *)param
+                         streamID:(NSString *)streamID;
+
+/// Custom video rendering local preview video frame CVPixelBuffer data callback.
+///
+/// Available since: 1.1.0
+/// Description: When [enableCustomVideoRender] is called to enable custom video rendering and the specified data type is [CVPixelBuffer], this function will be called back with the local preview video frame CVPixelBufferRef data.
+/// Use cases: Scenes that need to obtain video frame data captured by the SDK for special processing, such as cross-platform UI frameworks, game engines, etc..
+/// When to Trigger: After calling [startPreview], the SDK will call back when the local preview video frame data is captured.
+/// Related APIs: Call [setCustomVideoRenderHandler] to set custom video rendering callback.
+/// Platform differences: Only for iOS and macOS.
+///
+/// @param buffer Video frame data in CVPixelBufferRef
+/// @param param Video frame param.
+/// @param flipMode video flip mode.
+/// @param channel Publishing stream channel.
+- (void)onCapturedVideoFrameCVPixelBuffer:(CVPixelBufferRef)buffer
+                                    param:(ZGFlutterVideoFrameParam *)param
+                                 flipMode:(ZGFlutterVideoFlipMode)flipMode
+                                  channel:(ZGFlutterPublishChannel)channel;
+
+/// Custom video rendering remote playing stream video frame CVPixelBuffer data callback.
+///
+/// Available since: 1.1.0
+/// Description: When [enableCustomVideoRender] is called to enable custom video rendering and the specified data type is [CVPixelBuffer], this function will be called back with the remote playing stream video frame CVPixelBufferRef data.
+/// Use cases: Scenes that need to obtain the video frame data of the SDK remote playing stream for special processing, such as using a cross-platform UI framework, game engine, etc.
+/// When to Trigger: After calling [startPlayingStream], the SDK will call back when the remote streaming video frame data is received.
+/// Related APIs: Call [setCustomVideoRenderHandler] to set custom video rendering callback.
+/// Platform differences: Only for iOS and macOS.
+///
+/// @param buffer Video frame data in CVPixelBufferRef
+/// @param param Video frame param.
+/// @param streamID Stream ID.
+- (void)onRemoteVideoFrameCVPixelBuffer:(CVPixelBufferRef)buffer
+                                  param:(ZGFlutterVideoFrameParam *)param
+                               streamID:(NSString *)streamID;
+
+/// Custom video rendering remote playing stream video frame undecoded data callback.
+///
+/// Available since: 1.10.0
+/// Description: When [enableCustomVideoRender] is called to enable custom video rendering and the specified data type is [EncodedData], this function will be called back with the remote playing stream undecoded video frame data.
+/// When to Trigger: After calling [startPlayingStream], the SDK will call back when the remote streaming video frame data is received.
+/// Related APIs: Call [setCustomVideoRenderHandler] to set custom video rendering callback.
+///
+/// @param data Encoded data of video frames.
+/// @param dataLength Data length.
+/// @param param Video frame parameters.
+/// @param referenceTimeMillisecond video frame reference time, UNIX timestamp, in milliseconds.
+/// @param streamID Stream ID.
+- (void)onRemoteVideoFrameEncodedData:(unsigned char *_Nonnull)data
+                           dataLength:(unsigned int)dataLength
+                                param:(ZGFlutterVideoEncodedFrameParam *)param
+             referenceTimeMillisecond:(unsigned long long)referenceTimeMillisecond
+                             streamID:(NSString *)streamID;
+
+@end
+
+@interface ZegoCustomVideoRenderManager : NSObject
+
+/// Get the custom video capture manager instance
++ (instancetype)sharedInstance;
+
+/// Sets up the event callback handler for custom video rendering.
+///
+/// Available since: 1.9.0
+/// Description: Custom video rendering, where the developer is responsible for rendering the video data onto the UI components. With the change callback set, a callback that throws video data to the developer will be triggered when the developer calls startPreview, startPublishingStream, and startPlayingStream.
+/// Use case: Use beauty features or apps that use a cross-platform UI framework (for example, Qt requires a complex hierarchical UI to achieve high-experience interaction) or game engine (e.g. Unity, Unreal Engine, Cocos)
+/// When to call: After [createEngine].
+/// Related callbacks: [onCapturedVideoFrameRawData] local preview video frame data callback, [onRemoteVideoFrameRawData] remote playing stream video frame data callback.
+/// Note: This function is only available in ZegoExpressVideo SDK!
+///
+/// @param handler Custom video render handler
+-(void)setCustomVideoRenderHandler:(id<ZegoFlutterCustomVideoRenderHandler>)handler;
+
+-(id<ZegoFlutterCustomVideoRenderHandler>_Nullable)getCustomVideoRenderHandler;
+
+@end
+
+#endif /* ZegoCustomVideoRenderManager_h */
