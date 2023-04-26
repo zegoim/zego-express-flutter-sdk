f765fcc0a12d29cdfa76682a77a8b3981628317d
erchizeng <erchizeng@zego.im>
2023-04-26 18:05:02 +0800

    🔥 Remove unneeded code

--- android/src/main/java/im/zego/zego_express_engine/IZegoFlutterMediaPlayerVideoHandler.java
+++ android/src/main/java/im/zego/zego_express_engine/IZegoFlutterMediaPlayerVideoHandler.java
@@ -8,23 +8,6 @@ public abstract class IZegoFlutterMediaPlayerVideoHandler {
     /**
      * The callback triggered when the media player throws out video frame data.
      *
-     * Available since: 1.3.4
-     * Description: The callback triggered when the media player throws out video frame data.
-     * Trigger: The callback is generated when the media player starts playing.
-     * Caution: The callback does not actually take effect until call [setVideoHandler] to set.
-     * Restrictions: When playing copyrighted music, this callback will be disabled by default. If necessary, please contact ZEGO technical support.
-     *
-     * @param mediaPlayerIndex Callback player index.
-     * @param data Raw data of video frames (eg: RGBA only needs to consider data[0], I420 needs to consider data[0,1,2]).
-     * @param dataLength Data length (eg: RGBA only needs to consider dataLength[0], I420 needs to consider dataLength[0,1,2]).
-     * @param param Video frame flip mode.
-     */
-     public void onVideoFrame(int mediaPlayerIndex, ByteBuffer[] data, int[] dataLength,
-                             ZGFlutterVideoFrameParam param) {}
-
-    /**
-     * The callback triggered when the media player throws out video frame data.
-     *
      * Available since: 2.16.0
      * Description: The callback triggered when the media player throws out video frame data.
      * Trigger: The callback is generated when the media player starts playing.
